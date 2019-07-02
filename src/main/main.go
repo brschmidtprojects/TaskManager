f0021987a3dad1e825aa837ba234b3d3c8250e99
baileyschmidt <brschmidt@wpi.edu>
2019-07-02 17:22:28 -0400

    * Primitive test in main for status bar

--- src/main/main.go
+++ src/main/main.go
@@ -1,8 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	BASE = "["
+	FILL = "="
+	//CAP  = ">"
+	FIN = "] ... COMPLETE!"
+)
 
 func main() {
-
-	fmt.Println()
+	// initialize loop for testing terminal output
+	x := 0
+	// run loop for x
+	for x <= 100 {
+		switch x {
+		case 0:
+			fmt.Print(BASE)
+		case 100:
+			fmt.Print(FIN)
+		default:
+			fmt.Printf(FILL)
+		}
+		time.Sleep(time.Second / 25)
+		x += 1
+	}
 }
